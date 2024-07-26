abb9b30838cbbc554dda992553b88e3cc6174185
josephzenaw <yosephzenaw4@gmail.com>
2024-07-26 18:58:54 +0000

    Files Uploaded

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+// Create and append elements
+$(document).ready(function() {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  // Update counter function
+  let count = 0;
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }
+
+  // Bind debounce function from Lodash
+  const debouncedUpdateCounter = _.debounce(updateCounter, 300);
+  $('button').on('click', debouncedUpdateCounter);
+});
+
